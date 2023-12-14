87ea9b1ea6a9098aabaaa1ec30d9a52a2bdc03d6
Murylo Saladino <murylosaladino@gmail.com>
2023-12-13 23:05:56 -0300

    feat: cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,10 +3,12 @@ import { handleError } from './middlewares';
 import 'express-async-errors';
 import 'reflect-metadata';
 import express from 'express';
+import cors from 'cors';
 
 
 const app = express();
 app.use(express.json());
+app.use(cors)
 
 app.use('/users', usersRouter)
 app.use('/login', loginRouter)
